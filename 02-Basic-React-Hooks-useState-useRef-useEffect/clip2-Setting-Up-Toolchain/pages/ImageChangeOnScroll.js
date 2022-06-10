65af271c86ca1e50cd09db497ca99b9a2cde56e3
JonathanIntrica <jonathan@intrica.net>
2022-06-10 11:49:46 +0100

    Implement useEffect hook

--- 02-Basic-React-Hooks-useState-useRef-useEffect/clip2-Setting-Up-Toolchain/pages/ImageChangeOnScroll.js
+++ 02-Basic-React-Hooks-useState-useRef-useEffect/clip2-Setting-Up-Toolchain/pages/ImageChangeOnScroll.js
@@ -1,13 +1,27 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import ImageToggleOnScroll from "../src/ImageToggleOnScroll";
 
-
 const ImageChangeOnScroll = () => {
+
+    const [currentSpeakerId, setCurrentSpeakerId] = useState(0);
+    const [mouseEventCount, setMouseEventCount] = useState(0);
+
+    useEffect( () => {
+       window.document.title = `SpeakerId: ${currentSpeakerId}`;
+       console.log(`useEffect: setting title to ${currentSpeakerId}`);
+    }, [currentSpeakerId]);
+
     return (
         <div>
+            <span>mouseEventCnt: {mouseEventCount}</span>
             {[1124, 187, 823, 1269, 1530].map((speakerId) => {
                 return (
-                    <div key={speakerId}>
+                    <div key={speakerId}
+                        onMouseOver={() => {
+                            setCurrentSpeakerId(speakerId);
+                            setMouseEventCount(mouseEventCount + 1);
+                            console.log(speakerId);
+                        }}>
                         <ImageToggleOnScroll
                             primaryImage={`/static/speakers/bw/Speaker-${speakerId}.jpg`}
                             secondaryImage={`/static/speakers/Speaker-${speakerId}.jpg`}
