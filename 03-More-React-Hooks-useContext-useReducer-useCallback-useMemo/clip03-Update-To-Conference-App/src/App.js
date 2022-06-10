5659d80cd964ff26fb25f0d8484a384587d79ff0
JonathanIntrica <jonathan@intrica.net>
2022-06-10 14:26:39 +0100

    Implement speaker list with context

--- 03-More-React-Hooks-useContext-useReducer-useCallback-useMemo/clip03-Update-To-Conference-App/src/App.js
+++ 03-More-React-Hooks-useContext-useReducer-useCallback-useMemo/clip03-Update-To-Conference-App/src/App.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import Home from './Home.js';
+import Speakers from "./Speakers";
 
-const App = ({ pageName }) => {
-  if (pageName === 'Home') return <Home></Home>;
-  if (pageName === 'Speakers') return <></>;
-  return <div>Not Found</div>;
+export const ConfigContext = React.createContext();
+
+const pageToShow = pageName => {
+    if (pageName === "Home") return <Home/>;
+    if (pageName === "Speakers") return <Speakers/>;
+    return <div>Not Found</div>;
+};
+
+const configValue = {
+    showSignMeUp: true,
+    showSpeakerSpeakingDays: true,
+};
+
+const App = ({pageName}) => {
+    return (
+        <ConfigContext.Provider value={configValue}>
+            <div>{pageToShow(pageName)}</div>
+        </ConfigContext.Provider>
+    );
 };
 
 export default App;
