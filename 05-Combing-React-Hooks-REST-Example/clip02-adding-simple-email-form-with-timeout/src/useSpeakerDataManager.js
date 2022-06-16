75d7e3f13ed44774720666a225135ce79f9cd756
JonathanIntrica <jonathan@intrica.net>
2022-06-16 10:27:21 +0100

    Create api with read and write urls

--- 05-Combing-React-Hooks-REST-Example/clip02-adding-simple-email-form-with-timeout/src/useSpeakerDataManager.js
+++ 05-Combing-React-Hooks-REST-Example/clip02-adding-simple-email-form-with-timeout/src/useSpeakerDataManager.js
@@ -1,6 +1,6 @@
 import {useEffect, useReducer} from "react";
 import speakersReducer from "./speakersReducer";
-import SpeakerData from "./SpeakerData";
+import axios from "axios";
 
 function useSpeakerDataManager() {
     const [{isLoading, speakerList}, dispatch] = useReducer(speakersReducer, {
@@ -9,26 +9,37 @@ function useSpeakerDataManager() {
     });
 
     function toggleSpeakerFavorite(speakerRec) {
-        speakerRec.favorite === true ?
-            dispatch({type: "unfavorite", id: speakerRec.id}) :
-            dispatch({type: "favorite", id: speakerRec.id});
+        const updateData = async function () {
+            axios.put(`api/speakers/${speakerRec.id}`, {...speakerRec, favorite: !speakerRec.favorite,});
+            speakerRec.favorite === true
+                ? dispatch({type: "unfavorite", id: speakerRec.id})
+                : dispatch({type: "favorite", id: speakerRec.id});
+        };
+        updateData();
     }
 
     useEffect(() => {
-        new Promise(function (resolve) {
-            setTimeout(function () {
-                resolve();
-            }, 1000);
-        }).then(() => {
-            dispatch({
-                type: 'setSpeakerList',
-                data: SpeakerData,
-            });
-        });
+        //     new Promise(function (resolve) {
+        //         setTimeout(function () {
+        //             resolve();
+        //         }, 1000);
+        //     }).then(() => {
+        //         dispatch({
+        //             type: 'setSpeakerList',
+        //             data: SpeakerData,
+        //         });
+        //     });
+        let fetchData = async function () {
+            let result = await axios.get('api/speakers');
+            dispatch({type: 'setSpeakerList', data: result.data});
+        };
+        fetchData();
+
         return () => {
             console.log('cleanup');
         };
     }, []); // [speakingSunday, speakingSaturday]);
-    return { isLoading, speakerList, toggleSpeakerFavorite };
+    return {isLoading, speakerList, toggleSpeakerFavorite};
 }
+
 export default useSpeakerDataManager;
